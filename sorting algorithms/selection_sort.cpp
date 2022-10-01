51bb2a3e11af1527d1b04f1c7642721a16dcf1e4
Mayukh Sharma <86524409+mayukhsharma@users.noreply.github.com>
2022-10-01 14:32:59 +0530

    Create selection_sort.cpp
    selection sort in c++

---
+++ sorting algorithms/selection_sort.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cout<<"enter the size of array: ";
+cin>>n;
+int arr[n];
+cout<<"enter the value of array: ";
+for(int i=0;i<n;i++)
+{
+	cin>>arr[i];
+	}	
+	
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[j]<arr[i]){
+			int temp=arr[j];
+			arr[j]=arr[i];
+			arr[i]=temp;
+			}
+		}
+	}
+	cout<<"sorted array: ";
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<endl;
+	}
+return 0;
+}
